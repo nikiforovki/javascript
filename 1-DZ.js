5e9ee9c0b9b85c5cdd116bd9268e056f43821e07
nikiforovki <143290565+nikiforovki@users.noreply.github.com>
2025-06-24 10:55:03 +0300

    1-DZ

---
+++ 1-DZ.js
@@ -0,0 +1,13 @@
+const nameSity = 'Moscow'; // Названия города
+const temperature = 20; // Температура на сегодня
+const sityTemper = `${nameSity} +${temperature}`; // Конкатенация двух значений
+
+console.log(sityTemper) // Вывод в консоль результата
+
+
+const a = 5; // Первая переменная
+const b = 5; // Вторая переменная
+
+const multiplication = a * b; // Умножения двух переменных
+
+console.log(multiplication) // Вывод в консоль результата
