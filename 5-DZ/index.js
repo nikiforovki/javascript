b2c5762035e2716f2835e0482180303c90f1ce2c
nikiforovki <143290565+nikiforovki@users.noreply.github.com>
2025-06-26 11:57:43 +0300

    5-7DZ

---
+++ 5-DZ/index.js
@@ -0,0 +1,109 @@
+// // Задание 1
+//
+// const number = 2;
+//     if(number > 0) {
+//     console.log("Положительное число")
+//     }
+//
+//     if(number === 0) {
+//     console.log("Ноль")
+//     }
+//
+//     if(number < 0) {
+//     console.log("Отрицательное число")
+//     }
+//
+// // Задание 2
+// const height = prompt('Введите ваш рост (в см)');
+// const weight = prompt('Введите ваш вес (в кг)');
+//
+// if (height === null || weight === null || isNaN(height) || isNaN(weight)) {
+//     alert("Нужно ввести числа!");
+// } else {
+//     const heightInMeters = Number(height) / 100;
+//     const bmi = (Number(weight) / (heightInMeters * heightInMeters)).toFixed(2);
+//
+//     alert(`Ваш ИМТ: ${bmi}`);
+// }
+//  }
+//
+// // Задание 3
+// const monthNumber = Number(prompt('Введите номер месяца (1-12):'));
+// let monthName;
+//
+// switch (monthNumber) {
+//     case 1:
+//         monthName = 'Январь';
+//         break;
+//     case 2:
+//         monthName = 'Февраль';
+//         break;
+//     case 3:
+//         monthName = 'Март';
+//         break;
+//     case 4:
+//         monthName = 'Апрель';
+//         break;
+//     case 5:
+//         monthName = 'Май';
+//         break;
+//     case 6:
+//         monthName = 'Июнь';
+//         break;
+//     case 7:
+//         monthName = 'Июль';
+//         break;
+//     case 8:
+//         monthName = 'Август';
+//         break;
+//     case 9:
+//         monthName = 'Сентябрь';
+//         break;
+//     case 10:
+//         monthName = 'Октябрь';
+//         break;
+//     case 11:
+//         monthName = 'Ноябрь';
+//         break;
+//     case 12:
+//         monthName = 'Декабрь';
+//         break;
+//     default:
+//         monthName = 'Неверный номер месяца';
+// }
+// alert(monthName);
+// console.log(monthName);
+
+
+//Задание 4
+
+const monthNumber = Number(prompt('Введите номер месяца (1-12):'));
+let season;
+
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        season = 'Зима';
+        break;
+    case 3:
+    case 4:
+    case 5:
+        season = 'Весна';
+        break;
+    case 6:
+    case 7:
+    case 8:
+        season = 'Лето';
+        break;
+    case 9:
+    case 10:
+    case 11:
+        season = 'Осень';
+        break;
+    default:
+        season = 'Неверный номер! Введите 1-12';
+}
+
+alert(`Месяц ${monthNumber} — это ${season}!`);
+
---
+++ 7-DZ.js
@@ -0,0 +1,35 @@
+// Задание 1
+
+// const str = "JavaScript is fun!";
+// console.log(str.includes("fun"))
+
+// Задание 2
+
+// const a = 4;
+// const b = "Привет"; // "JS' выведет tru
+//     if (a % b.length === 0) {
+//     console.log(true);
+//     } else {
+//     console.log(false);
+//     }
+
+// Задание 3
+
+// const firstName = "Ivan"
+// const lastName = "Petrov"
+// const occupation = "software developer"
+//
+// const str =`Hello, my name is ${firstName} ${lastName + "."} I am a ${occupation}`
+//
+// console.log(str)
+
+// Задание 4
+
+// console.log(null == undefined) // В JS null и undefined равны только друг другу при нестрогом сравнении.
+// console.log(null === undefined) // При строгом сравнение сравниваются типы данных
+
+// console.log(1 == '1') // Будет true. При не строгом сравнение без разницы какой тип.
+//
+// console.log(1 === '1') // При строгом сравнение сравниваются типы данных
+
+
